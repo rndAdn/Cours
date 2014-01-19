41823a6f9bae74edd16cf76672d6d89b45b2790d
Adequin Renaud <adequin.renaud@gmail.com>
2014-01-19 11:03:36 +0100

    maj

--- L2/IK3_2014_Langage_Java/DenkiBlocks/Denki_Blocks/src/moteur/Profile.java
+++
@@ -1,4 +0,0 @@
-package moteur;
-
-public class Profile {
-}
--- L2/LC4_2012_Langage_C/TP1/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	fputs("pouet\n",stdout); /* La fontion fputs affiche le message "pouet\n" sur "stdout" (sortie standard)*/
-	return 0;
-}
