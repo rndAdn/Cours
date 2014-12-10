5fa3d1e1e1fc0f051c83c550ff9a9a9cb8fcde87
Toussaint Etienne <tsst.etienne@gmail.com>
2014-12-10 21:01:01 +0100

    :)

--- L3/S5/POO-IG5/Etienne/TP9/src/Main.java
+++ L3/S5/POO-IG5/Etienne/TP9/src/Main.java
@@ -2,14 +2,7 @@
 public class Main {
 
 	public static void main(String [] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				ImageEdit view = new ImageEdit();
-				view.pack();
-				view.setVisible(true);
-				view.repaint();
-			}
-		});
+		System.out.println("Hey");
 	}
 
 }
