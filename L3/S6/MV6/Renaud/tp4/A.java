19ed1df5c546bed7e8257e85b83afb4ec81c027b
Adequin Renaud <adequin.renaud@gmail.com>
2015-04-16 11:36:18 +0200

    add LICENCE

---
+++ L3/S6/MV6/Renaud/tp4/A.java
@@ -0,0 +1,47 @@
+class A{
+
+    /*public int f(int x, float y){
+        int i = x + 2;
+        return i;
+    }*/
+
+    /*public int f(int x, float y){
+        int j;
+        int i = 3;
+        j = i + 2;
+        return j + x;
+    }*/
+
+    /*public int f(int x, float y){
+        float a = x + y;
+        return 3;
+    }*/
+
+    /*public int f(int x, float y){
+        int a,b,c;
+        b = 0;
+        c = 2;
+        a = 33;
+
+        if(a == c){
+            return b;
+        }
+        b++;
+        return b;
+    }*/
+
+
+    public int f(int x, float y){
+
+        int a,b;
+        a = 3;
+        b = 0;
+        while(b < x){
+            a += 11;
+            b++;
+        }
+        return a;
+    }
+
+
+}
